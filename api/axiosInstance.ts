d6c5f146ee91e41bb7359725e61fd7afbae25843
Victor Kamau <kamaukaris02@gmail.com>
2025-10-09 11:49:35 +0300

    axios instance

--- api/axiosInstance.ts
+++ api/axiosInstance.ts
@@ -7,5 +7,15 @@ const api = axios.create({
     'Content-Type': 'application/json',
   },
 });
+const API_BASE_URL =
+  import.meta.env.MODE === 'development'
+    ? 'http://localhost:5000/api'
+    : 'https://starose-backend.onrender.com/api';
 
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
 export default api;
