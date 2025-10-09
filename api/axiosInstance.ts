e5f550bad2b7726f55f45cd6ef7c0af9269cd3a2
Victor Kamau <kamaukaris02@gmail.com>
2025-10-09 12:48:34 +0300

    double api error

--- api/axiosInstance.ts
+++ api/axiosInstance.ts
@@ -1,10 +1,9 @@
-// src/api/axiosInstance.ts
 import axios from 'axios';
 
 const API_BASE_URL =
   import.meta.env.MODE === 'development'
     ? 'http://localhost:5000/api'
-    : 'https://starose-backend.onrender.com/api'; // <-- notice /api at the end
+    : 'https://starose-backend.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
